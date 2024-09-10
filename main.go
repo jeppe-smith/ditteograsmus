066240b7b7709a27a5f2c06dff1681b7a2d4c975
Jeppe Smith <mail@jeppesmith.dk>
2024-09-10 13:51:46 +0200

    use uploads again

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 			return
 		}
 
-		files, err := ioutil.ReadDir("public")
+		files, err := ioutil.ReadDir("uploads")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -58,7 +58,7 @@ func main() {
 			}
 			defer file.Close()
 
-			tempFile, err := ioutil.TempFile("public", "*.webp")
+			tempFile, err := ioutil.TempFile("uploads", "*.webp")
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
@@ -82,7 +82,9 @@ func main() {
 		}
 	})
 
+	uploads := http.FileServer(http.Dir("./uploads"))
 	public := http.FileServer(http.Dir("./public"))
+	http.Handle("/uploads/", http.StripPrefix("/uploads/", uploads))
 	http.Handle("/public/", http.StripPrefix("/public/", public))
 
 	log.Fatal(http.ListenAndServe(":1337", nil))
